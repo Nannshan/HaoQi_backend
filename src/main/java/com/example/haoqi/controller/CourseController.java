206cb8bfab0eb4a822064a96e7fa294c6f33151d
righthan <2753875841@qq.com>
2023-12-04 17:03:33 +0800

    课程查询修改

--- src/main/java/com/example/haoqi/controller/CourseController.java
+++ src/main/java/com/example/haoqi/controller/CourseController.java
@@ -6,10 +6,7 @@ import com.example.haoqi.entity.Course;
 import com.example.haoqi.mapper.CourseMapper;
 import com.example.haoqi.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/course")
@@ -17,7 +14,7 @@ public class CourseController {
     @Autowired
     private CourseMapper courseMapper;
 
-    @PostMapping("/queryPage")
+    @GetMapping("/queryPage")
     public Result queryPage(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int pageSize, String courseName) {
         QueryWrapper<Course> queryWrapper = new QueryWrapper<>();
         Page<Course> coursePage = new Page<>(page, pageSize);
